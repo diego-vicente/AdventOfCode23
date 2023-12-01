d3aae859426582a1f233685cbde76adeecadabf1
Diego Vicente <dvicente@carto.com>
2023-12-01 12:43:27 +0100

    Add solution for the second part of Day 1

--- Sources/Solutions/Day01.swift
+++ Sources/Solutions/Day01.swift
@@ -10,16 +10,75 @@ struct Calibration {
     self.line = line.trimmingCharacters(in: .whitespacesAndNewlines)
   }
 
+  /// Compute the calibration value for a given digit input.
+  ///
+  /// Returns the value of concatenating the first and last digit of a string.
+  /// If there are no digits in the input, zero is returned.
+  ///
+  /// - Parameter input: string to be processed
+  /// - Returns: the integer obtained by concatenating the first and last digit.
+  private func valueWithDigits(_ input: String) -> Int {
+    let digits = input.filter("0123456789".contains)
+    guard digits.count > 0 else { return 0 }
+    return Int(digits.prefix(1) + digits.suffix(1))!
+  }
+
   /// Returns the calibration value.
   ///
-  /// The calibration value is computed using the first and the last digit
-  /// present in the original input line and concatenating them together.
+  /// The calibration value seemed to be computed using the first and the last
+  /// digit present in the original input line and concatenating them together.
   ///
-  /// - Returns: The calibrated value.
-  func value() -> Int {
-    let digits = line.filter("0123456789".contains)
-    let value = Int(digits.prefix(1) + digits.suffix(1))!
-    return value
+  /// - Returns: The calibration value.
+  func wrongValue() -> Int {
+    return valueWithDigits(line)
+  }
+
+  /// Substitutes number literals with their digit representation.
+  ///
+  /// - Parameter input: The input to be processed.
+  /// - Returns: The input with the literal values substituted.
+  private func processInput(_ input: String) -> String {
+    let numberLiterals = [
+      ("zero", "0"),
+      ("one", "1"),
+      ("two", "2"),
+      ("three", "3"),
+      ("four", "4"),
+      ("five", "5"),
+      ("six", "6"),
+      ("seven", "7"),
+      ("eight", "8"),
+      ("nine", "9"),
+    ]
+
+    var unprocessed = input
+    var digits = [String]()
+
+    while !unprocessed.isEmpty {
+      for (literal, digit) in numberLiterals {
+        if unprocessed.hasPrefix(digit) || unprocessed.hasPrefix(literal) {
+          digits.append(digit)
+          break
+        }
+      }
+
+      unprocessed = String(unprocessed.dropFirst())
+    }
+
+    return digits.joined()
+  }
+
+  /// Returns the calibration value.
+  ///
+  /// For a given input line, the actual calibration value is computed by
+  /// concatenating the first and the last digit present in the input line.
+  /// However, the input line may contain number literals (for instance, "one"
+  /// or "1").
+  ///
+  /// - Returns: the calibration value.
+  func correctValue() -> Int {
+    let processed = processInput(line)
+    return valueWithDigits(processed)
   }
 }
 
@@ -35,9 +94,11 @@ struct Input {
   /// - Parameter path: The path to the file containing the input.
   init(path: String) {
     let contents = try! String(contentsOfFile: path)
-    let calibrations = contents.split(separator: "\n").map { line in
-      Calibration(String(line))
-    }
+    let calibrations =
+      contents
+      .split(separator: "\n")
+      .filter { !$0.isEmpty }
+      .map { line in Calibration(String(line)) }
 
     self.init(calibrations)
   }
@@ -64,7 +125,24 @@ public class Day01: Solution {
     let input = Input(path: inputPath)
 
     let result = input.calibrations
-      .map { $0.value() }
+      .map { $0.wrongValue() }
+      .reduce(0, +)
+
+    return String(result)
+  }
+
+  /// Computes the solution for the second part of Day 1.
+  ///
+  /// This solution is the sum of all the calibration values provided in the
+  /// input, but the calibration value now also takes number literal into
+  /// account.
+  ///
+  /// - Returns: The computed solution as a string.
+  override func secondPart() -> String {
+    let input = Input(path: inputPath)
+
+    let result = input.calibrations
+      .map { $0.correctValue() }
       .reduce(0, +)
 
     return String(result)
