e2f3ca15dbcc55e1cee9ef4f1fea9198a9d41df4
Diego Vicente <dvicente@carto.com>
2023-12-02 13:03:20 +0100

    Configure package for macOS 10.13 or later

--- Package.swift
+++ Package.swift
@@ -4,17 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdventOfCode23",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "AdventOfCode23",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
-    ]
+  name: "AdventOfCode23",
+  platforms: [.macOS(.v13)],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "AdventOfCode23",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ])
+  ]
 )
