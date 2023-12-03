27fcdad0076d69e46ecfc43f1ad6b2cba28c1ea2
Diego Vicente <dvicente@carto.com>
2023-12-03 22:01:38 +0100

    Add a solution for the second part of Day 3

--- Sources/Solutions/Day03.swift
+++ Sources/Solutions/Day03.swift
@@ -38,23 +38,32 @@ private struct Point: Hashable, CustomStringConvertible {
 
 /// An engine number is a number in the grid.
 private struct EngineNumber: CustomStringConvertible {
-  let value: Int
+  let number: Int
   let start: Point
 
-  var description: String { return "\(value) at \(start)" }
+  var description: String { return "\(number) at \(start)" }
 
   var neighbors: Set<Point> {
-    return start.neighbors(length: String(value).count)
+    return start.neighbors(length: String(number).count)
   }
 }
 
 /// An engine symbol is a symbol in the grid.
-private struct EngineSymbol: CustomStringConvertible {
+private struct EngineSymbol: Hashable, CustomStringConvertible {
   let symbol: Character
   let start: Point
 
   var description: String { return "\(symbol) at \(start)" }
   var neighbors: Set<Point> { return start.neighbors() }
+
+  static func == (lhs: EngineSymbol, rhs: EngineSymbol) -> Bool {
+    return lhs.symbol == rhs.symbol && lhs.start == rhs.start
+  }
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(symbol)
+    hasher.combine(start)
+  }
 }
 
 /// The input is a representation of the grid.
@@ -97,7 +106,7 @@ private struct Input {
 
           // Add the existing number to the list
           let number = Int(numberPartials.joined())!
-          numbers[start] = EngineNumber(value: number, start: start)
+          numbers[start] = EngineNumber(number: number, start: start)
           numberPartials = []
         }
       }
@@ -128,7 +137,38 @@ public class Day03: Solution {
 
     let result = input.numbers.values
       .filter { n in n.neighbors.contains { input.symbols[$0] != nil } }
-      .reduce(0) { $0 + $1.value }
+      .reduce(0) { $0 + $1.number }
+
+    return String(result)
+  }
+
+  /// Computes the solution for the second part of Day 3.
+  ///
+  /// The solution for the second part is filtering all gears (asterisks
+  /// contiguous to exactly two numbers), find their gear ratio (multiplying the
+  /// numbers together) and adding all of them together.
+  ///
+  /// - Returns: the solution as a string.
+  override func secondPart() throws -> String {
+    let input = Input(path: inputPath)
+
+    // Map each symbol to their contiguous numbers
+    var candidates: [EngineSymbol: [EngineNumber]] = [:]
+    for number in input.numbers.values {
+      let neighbors = number.neighbors
+      for symbol in neighbors.compactMap({ input.symbols[$0] }) {
+        if candidates[symbol] == nil { candidates[symbol] = [] }
+        candidates[symbol]?.append(number)
+      }
+    }
+
+    // Filter the gears and compute the result
+    let result =
+      candidates
+      .filter { $0.key.symbol == "*" && $0.value.count == 2 }
+      .values
+      .map { $0[0].number * $0[1].number }
+      .reduce(0, +)
 
     return String(result)
   }
